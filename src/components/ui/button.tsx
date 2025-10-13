29ffb8cabc3883c13e06572294de0e3ce5b72208
iyiolacak <iyiolacak0@gmail.com>
2025-10-13 17:15:59 +0300

    new shadcn components and new styling so we keep consistency between the UI and visual guidelines of Local Do Rush

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 import { useHoverClickSounds } from "@/lib/sfx"
 
 const buttonVariants = cva(
-  "inline-flex dark:h-11 active:scale-95 cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-sm text-lg font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex dark:h-11 active:scale-95 cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-lg text-lg font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
